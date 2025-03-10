40627928c7e38d0fe922614694535a69e9ae1334
BelalBasal <basdibrahim@gmail.com>
2025-03-10 15:56:36 +0200

    sweetalert

--- SchoolManagmentSystemDAL/Migrations/20250310134059_v08.Designer.cs
+++ SchoolManagmentSystemDAL/Migrations/20250310134059_v08.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolManagmentSystem.PL.Data;
 
 #nullable disable
 
-namespace SchoolManagementSystemDAL.Migrations
+namespace SchoolManagmentSystemDAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250310134059_v08")]
+    partial class v08
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SchoolManagmentSystemDAL/Migrations/20250310134059_v08.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolManagmentSystemDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class v08 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classes_AspNetUsers_ManagerId",
+                table: "Classes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classes_Teachers_ManagerId",
+                table: "Classes",
+                column: "ManagerId",
+                principalTable: "Teachers",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classes_Teachers_ManagerId",
+                table: "Classes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classes_AspNetUsers_ManagerId",
+                table: "Classes",
+                column: "ManagerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
